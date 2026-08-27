f859a0e74cf6fbd90faa1cbabbfef4e8d2928ff2
Pavlos Louk <pavloz@proton.me>
2026-08-27 18:49:30 +0300

    Add .config/nvim/lua/plugins/dap-python.lua

---
+++ private_dot_config/nvim/lua/plugins/dap-python.lua
@@ -0,0 +1,31 @@
+return {
+  {
+    "mfussenegger/nvim-dap-python",
+    ft = "python",
+    dependencies = {
+      "mfussenegger/nvim-dap",
+    },
+    config = function()
+      local dap = require("dap")
+      local dap_python = require("dap-python")
+
+      -- Initialize dap-python runner
+      local venv = os.getenv("VIRTUAL_ENV")
+      local python_path = venv and (venv .. "/bin/python") or "python"
+      dap_python.setup(python_path)
+
+      -- Hardcode the ONLY debug target to Attach
+      dap.configurations.python = {
+        {
+          type = "python",
+          request = "attach",
+          name = "Attach to Running App (5678)",
+          connect = {
+            host = "127.0.0.1",
+            port = 5678,
+          },
+        },
+      }
+    end,
+  },
+}
